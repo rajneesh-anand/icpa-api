292bfca50d0010f4db28d9eabf8fe29dc505d976
rajneesh.k.anand@gmail.com <rajneesh.k.anand@gmail.com>
2021-12-05 19:03:19 +0530

    about page

--- routes/upload.js
+++ routes/upload.js
@@ -93,6 +93,35 @@ router.post("/category", async (req, res) => {
   }
 });
 
+router.post("/about", async (req, res) => {
+  const data = await new Promise((resolve, reject) => {
+    const form = new IncomingForm();
+    form.parse(req, async (err, fields, files) => {
+      if (err) return reject(err);
+      resolve({ fields, files });
+    });
+  });
+
+  const rawData = fs.readFileSync(data.files.uploadedFile.path);
+
+  try {
+    fs.writeFile(
+      path.join(__dirname, `../upload/${data.files.uploadedFile.name}`),
+      rawData,
+      function (err) {
+        if (err) console.log(err);
+        return res.status(200).json({
+          status: "success",
+          message: "File uploaded successfully",
+        });
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.json(error);
+  }
+});
+
 router.get("/franchise", async (req, res) => {
   res.header("Content-Type", "application/json");
   res.sendFile(path.join(__dirname, "../upload/franchise.json"));
@@ -106,6 +135,11 @@ router.get("/plan", async (req, res) => {
 router.get("/category", async (req, res) => {
   res.header("Content-Type", "application/json");
   res.sendFile(path.join(__dirname, "../upload/product-category.json"));
+});
+
+router.get("/about", async (req, res) => {
+  res.header("Content-Type", "application/json");
+  res.sendFile(path.join(__dirname, "../upload/about.json"));
 });
 
 module.exports = router;
